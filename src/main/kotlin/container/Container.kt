5d11eec9a3bfd4b861453f4e93b373d6e4c9db97
Ruslan Borysov <borysov.ruslan98@gmail.com>
2024-03-02 10:22:59 +0200

    added documentation & comments to container classes

--- src/main/kotlin/container/Container.kt
+++ src/main/kotlin/container/Container.kt
@@ -1,9 +1,41 @@
 package com.ruslanb.dao
 
 interface Container<T> {
+    /**
+     * Inserts a new model instance into the container.
+     *
+     * @param model The model instance to be inserted.
+     */
     fun append(model: T)
+
+    /**
+     * Removes a model instance from the container by its index.
+     *
+     * @param index The index of the model to be removed.
+     * @return The recently removed model instance.
+     */
     fun remove(index: Int): T
+
+    /**
+     * Updates an existing model instance in the container by its index.
+     *
+     * @param model The model instance with updated fields that should replace the existing one.
+     * @param index The index of the model to be updated.
+     */
     fun update(model: T, index: Int)
+
+    /**
+     * Retrieves a model instance in the container by its index.
+     *
+     * @param index The index of the model to get.
+     * @return The retrieved model instance.
+     */
     fun get(index: Int): T
+
+    /**
+     * Retrieves all model instances in the container.
+     *
+     * @return The list of model instances in the container.
+     */
     fun getAll(): List<T>
 }
--- src/main/kotlin/container/ListContainerImpl.kt
+++ src/main/kotlin/container/ListContainerImpl.kt
@@ -6,26 +6,32 @@ open class ListContainerImpl<T>(
     private val models = mutableListOf<T>()
 
     init {
+        // fills `models` list with initial models
         init?.forEach(models::add)
     }
 
     override fun append(model: T) {
+        // adds model instance to the `models` list
         models.add(model)
     }
 
     override fun remove(index: Int): T {
+        // removes model instance from the `models` list by its index
         return models.removeAt(index)
     }
 
     override fun get(index: Int): T {
+        // retrieves model instance from the `models` list by its index
         return models[index]
     }
 
     override fun getAll(): List<T> {
+        // returns the copy of `models` list
         return models.toList()
     }
 
     override fun update(model: T, index: Int) {
+        // updates model instance at index in the `models` list
         models[index] = model
     }
 }
